cb670bc254b73479c487d45207dedda93ef06588
Rémi Parmentier <remi@hteumeuleu.fr>
2023-05-11 09:18:52 +0200

    feat: add level

--- Source/Scripts/Game.lua
+++ Source/Scripts/Game.lua
@@ -1,18 +1,14 @@
 class("Game").extends()
 
 local gfx <const> = playdate.graphics
-local ldtk <const> = LDtk
-local offset <const> = playdate.geometry.point.new(10, 10)
 local font <const> = gfx.font.new("Assets/closed")
 gfx.setFont(font)
-
-ldtk.load("Levels/world.ldtk", false)
 
 function Game:init()
 
 	Game.super.init(self)
 	self:attachEvents()
-	self:goToLevel("Level_0")
+	self.level = Level(0)
 	self:load()
 	return self
 
@@ -25,73 +21,23 @@ function Game:attachEvents()
 	local myInputHandlers = {
 
 		leftButtonDown = function()
-			self.player:left()
+			self.level.player:left()
 		end,
 		rightButtonDown = function()
-			self.player:right()
+			self.level.player:right()
 		end,
 		upButtonDown = function()
-			self.player:up()
+			self.level.player:up()
 		end,
 		downButtonDown = function()
-			self.player:down()
+			self.level.player:down()
 		end,
 		BButtonDown = function()
-			self.player:back()
+			self.level.player:back()
 		end,
 
 	}
 	playdate.inputHandlers.push(myInputHandlers)
-
-end
-
--- goToLevel()
---
--- based on SquidGodDev’s video tutorial.
-function Game:goToLevel(level_name)
-
-	gfx.sprite.removeAll()
-
-	for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
-		if layer.tiles then
-			local tilemap = ldtk.create_tilemap(level_name, layer_name)
-			local layerSprite = gfx.sprite.new()
-			layerSprite:setTilemap(tilemap)
-			layerSprite:setCenter(0, 0)
-			layerSprite:moveTo(offset.x, offset.y)
-			layerSprite:setZIndex(layer.zIndex)
-			layerSprite:add()
-
-			local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
-			if emptyTiles then
-				gfx.sprite.addWallSprites(tilemap, emptyTiles, offset.x, offset.y)
-			end
-		end
-	end
-
-	-- Level outer walls
-	gfx.sprite.addEmptyCollisionSprite(0, 0, 400, 10)
-	gfx.sprite.addEmptyCollisionSprite(0, 230, 400, 10)
-	gfx.sprite.addEmptyCollisionSprite(0, 10, 10, 220)
-	gfx.sprite.addEmptyCollisionSprite(390, 10, 10, 220)
-
-	-- Entities
-	for index, entity in ipairs(LDtk.get_entities(level_name)) do
-		if entity.name == "Light" then
-			Light(entity.position.x + offset.x, entity.position.y + offset.y)
-		elseif entity.name == "Battery" then
-			self.battery = Battery(entity.position.x + offset.x, entity.position.y + offset.y)
-		elseif entity.name == "Player" then
-			self.player = Player(entity.position.x + offset.x, entity.position.y + offset.y)
-		end
-	end
-
-	if self.battery and self.player then
-
-		self.player:attachBattery(self.battery)
-
-	end
-
 
 end
 
---
+++ Source/Scripts/Level.lua
@@ -0,0 +1,67 @@
+class('Level').extends()
+
+local gfx <const> = playdate.graphics
+local ldtk <const> = LDtk
+ldtk.load("Levels/world.ldtk", false)
+local offset <const> = playdate.geometry.point.new(10, 10)
+
+-- Level
+--
+function Level:init(index)
+
+	self.name = "Level_" .. index
+	self:load()
+
+end
+
+-- load()
+--
+-- based on SquidGodDev’s video tutorial.
+function Level:load()
+
+	gfx.sprite.removeAll()
+
+	local level_name = self.name
+
+	for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
+		if layer.tiles then
+			local tilemap = ldtk.create_tilemap(level_name, layer_name)
+			local layerSprite = gfx.sprite.new()
+			layerSprite:setTilemap(tilemap)
+			layerSprite:setCenter(0, 0)
+			layerSprite:moveTo(offset.x, offset.y)
+			layerSprite:setZIndex(layer.zIndex)
+			layerSprite:add()
+
+			local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
+			if emptyTiles then
+				gfx.sprite.addWallSprites(tilemap, emptyTiles, offset.x, offset.y)
+			end
+		end
+	end
+
+	-- Level outer walls
+	gfx.sprite.addEmptyCollisionSprite(0, 0, 400, 10)
+	gfx.sprite.addEmptyCollisionSprite(0, 230, 400, 10)
+	gfx.sprite.addEmptyCollisionSprite(0, 10, 10, 220)
+	gfx.sprite.addEmptyCollisionSprite(390, 10, 10, 220)
+
+	-- Entities
+	for index, entity in ipairs(LDtk.get_entities(level_name)) do
+		if entity.name == "Light" then
+			Light(entity.position.x + offset.x, entity.position.y + offset.y)
+		elseif entity.name == "Battery" then
+			self.battery = Battery(entity.position.x + offset.x, entity.position.y + offset.y)
+		elseif entity.name == "Player" then
+			self.player = Player(entity.position.x + offset.x, entity.position.y + offset.y)
+		end
+	end
+
+	if self.battery and self.player then
+
+		self.player:attachBattery(self.battery)
+
+	end
+
+
+end
